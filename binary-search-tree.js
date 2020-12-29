dab5a24f9fc58abb8cd21f78d8792e55d8850e4c
Jeremy Langevin <langevinj832@gmail.com>
2020-12-29 12:07:00 -0500

    Both inserts and find methods created

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -15,28 +15,79 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
+    let newNode = new Node(val);
+    //if the tree doesn't have a root, set the newNode as the root
+    if(this.root === null){
+      this.root = newNode;
+      return this;
+    }
 
+    let curr = this.root;
+    let parent;
+
+    while(curr) {
+      parent = curr;
+      newNode.val < curr.val ? curr = curr.left : curr = curr.right;
+    }
+
+    newNode.val < parent.val ? parent.left = newNode : parent.right = newNode
+    return this
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
   insertRecursively(val) {
+    let newNode = new Node(val);
 
+    function _insertHelper(node, curr=null) {
+      if(node !== null) curr = node
+      if(node === null) return curr
+      if(node.left === null && node.right === null) return curr;
+
+      let parent = newNode.val < node.val ? _insertHelper(node.left, curr) : _insertHelper(node.right, curr);
+
+      return parent;
+    }
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let p = _insertHelper(this.root)
+      p.val > newNode.val ? p.left = newNode : p.right = newNode;
+    }
+    return this;
   }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
+    let current = this.root;
 
+    while(current) {
+      if(current.val === val) return current;
+
+      current = val < current.val ? current.left : current.right;
+    }
+    return undefined;
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
   findRecursively(val) {
+    if(this.root === null) return undefined;
 
+    function _findHelper(node){
+      //base cases
+      if(node === null) return undefined;
+      if(node.val === val) return node;
+
+      return val < node.val ? _findHelper(node.left) : _findHelper(node.right);
+    }
+
+    return _findHelper(this.root)
   }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
